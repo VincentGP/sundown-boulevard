0d61b8df5e556fc46d5208f4b64c112a1784efdb
Vincent Gercke <vgercke@gmail.com>
2021-04-16 22:48:49 +0200

    Initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,30 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    currentOrder: {
+      selectedDish: null,
+      selectedBeverages: [],
+      dateAndTime: new Date(),
+      amountOfPeople: 0,
+      email: ''
+    }
   },
   mutations: {
+    toggleBeverageSelection(state, beverageData) {
+      const index = state.currentOrder.selectedBeverages.findIndex(x => x.id == beverageData.id);
+      index === -1 ? state.currentOrder.selectedBeverages.push(beverageData) : state.currentOrder.selectedBeverages.splice(index);
+    }
   },
   actions: {
+    updateOrder({commit}, orderData) {
+
+    }
   },
-  modules: {
+  getters: {
+    getTodoById: (state) => (id) => {
+      return state.todos.find(todo => todo.id === id)
+    }
   }
+  // modules: {
+  // }
 })
